6e60bdc6a926ccaf30d49b728e32a6f9f56b3548
Naoya Yamashita <conao3@gmail.com>
2023-04-05 00:36:51 +0900

    show widgets with ListView

--- src/parts/CottonSidebar.tsx
+++ src/parts/CottonSidebar.tsx
@@ -1,7 +1,26 @@
-import { View } from "@adobe/react-spectrum";
+import { Item, ListView, View } from "@adobe/react-spectrum";
 
 export default function CottonSidebar() {
+  const items = [
+    {id: 'label', label: 'Label'},
+    {id: 'textbox', label: 'Textbox'},
+    {id: 'checkbox', label: 'Checkbox'},
+    {id: 'dropdown', label: 'Dropdown'},
+    {id: 'attachment', label: 'Attachment'},
+    {id: 'datetime', label: 'Datetime'},
+    {id: 'spacer', label: 'Spacer'},
+    {id: 'divider', label: 'Divider'},
+  ];
+
   return (
-    <View backgroundColor="blue-600" height="100%" />
+    <View height="100%" >
+      <ListView>
+        {
+          items.map(item => (
+            <Item key={item.id}>{item.label}</Item>
+          ))
+        }
+      </ListView>
+    </View>
   )
 }
